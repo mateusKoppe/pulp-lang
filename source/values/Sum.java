1fd69b027158aeb479917e96d7bffa2867f360cf
Eduardo Sabino <eduardoaikin@gmail.com>
2019-10-22 22:18:35 -0300

    fix: syntax to runtime

--- source/values/Sum.java
+++ source/values/Sum.java
@@ -17,7 +17,7 @@ public class Sum extends Value {
         try {
             return Double.toString(Double.parseDouble(this.args[0].getValue()) + Double.parseDouble(this.args[1].getValue()));
         } catch(NumberFormatException e) {
-            throw new Exception("Operation \"sum\" got and syntax error. \n" +
+            throw new Exception("Operation \"sum\" got an runtime error. \n" +
                 "You must pass a number to make sum operations, to concat strings use 'concat'"
             );
         }
